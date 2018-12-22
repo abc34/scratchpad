42036d9630a131397badee6c614a994a2aa0bde1
abc34 <mail4p@ya.ru>
2018-12-22 20:05:45 +0400

    Update bignumber_user_defined.h

--- bignumber_user_defined.h
+++ bignumber_user_defined.h
@@ -43,12 +43,15 @@ double _log(mbedtls_mpi *X);
 size_t mbedtls_mpi_lsb(const mbedtls_mpi *X);
 size_t mbedtls_clz(mbedtls_mpi_uint x);
 inline int mbedtls_mpi_get_bit(const mbedtls_mpi *X, size_t pos);
+int mbedtls_mpi_shift_l(mbedtls_mpi *X, size_t count);
+int mbedtls_mpi_shift_r(mbedtls_mpi *X, size_t count);
 int mbedtls_mpi_write_string(const mbedtls_mpi *X, int radix, char *buf, size_t buflen, size_t *olen);
 //mbedtls_mpi_mod_int() replaced by mbedtls_mpi_mod_uint()
 //new added
 int mbedtls_udbl_div_uint(const mbedtls_t_udbl *X, const mbedtls_mpi_uint y, mbedtls_t_udbl *Q, mbedtls_mpi_uint *r);
 int mbedtls_mpi_div_uint(mbedtls_mpi* X, mbedtls_mpi_uint y, mbedtls_mpi_uint *r);
 int mbedtls_mpi_mod_uint(mbedtls_mpi_uint *r, const mbedtls_mpi *A, mbedtls_mpi_uint b);
+int mbedtls_mpi_sqrt_i(mbedtls_mpi *X, const mbedtls_mpi *A);
 int mbedtls_mpi_pow(mbedtls_mpi *X, const mbedtls_mpi *A, const mbedtls_mpi *E);
 int mbedtls_mpi_mod2n(mbedtls_mpi *X, const mbedtls_mpi *A, const mbedtls_mpi_sint n);
 int mbedtls_mpi_mulmod2n(mbedtls_mpi *X, const mbedtls_mpi *A, const mbedtls_mpi *B, const mbedtls_mpi_sint n);
@@ -56,8 +59,8 @@ int mbedtls_mpi_invmod2n(mbedtls_mpi *X, const mbedtls_mpi *A, const mbedtls_mpi
 int mbedtls_mpi_expmod2n(mbedtls_mpi *X, const mbedtls_mpi *A, const mbedtls_mpi *E, const mbedtls_mpi_sint n);
 int mbedtls_mpi_expmod_full(mbedtls_mpi *X, const mbedtls_mpi *A, const mbedtls_mpi *E, const mbedtls_mpi *N);
 //int mbedtls_mpi_inv_mod_DIV(mbedtls_mpi *X, const mbedtls_mpi *A, const mbedtls_mpi *N);
-int mbedtls_mpi_inv_mod_hlp(mbedtls_mpi *X, const mbedtls_mpi *A, const mbedtls_mpi *B, const mbedtls_mpi *C);
-int mbedtls_mpi_inv_mod_full(mbedtls_mpi *X, const mbedtls_mpi *A, const mbedtls_mpi *N, const mbedtls_mpi *C);
+int mbedtls_mpi_invmod_hlp(mbedtls_mpi *X, const mbedtls_mpi *A, const mbedtls_mpi *B, const mbedtls_mpi *C);
+int mbedtls_mpi_invmod_full(mbedtls_mpi *X, const mbedtls_mpi *A, const mbedtls_mpi *N, const mbedtls_mpi *C);
 
 #ifdef __cplusplus
 }
